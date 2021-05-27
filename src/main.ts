82e6d335f67b7040db519316c2817226f1647807
Erik Gonzalez <erikhuboy@gmail.com>
2021-05-26 22:44:05 -0500

    export as test

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,2 @@
-export * from './components/HelloWorld.vue';
-export * from './components/Counter.vue';
+export * as HelloWorld from './components/HelloWorld.vue';
+export * as Counter from './components/Counter.vue';
