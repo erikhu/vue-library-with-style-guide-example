7b4c25397bc04bd141313975555def21e1e06097
Erik Gonzalez <erikhuboy@gmail.com>
2021-05-26 22:20:40 -0500

    new counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>&#127914; Awesome counter &#10084;</h2>
+    <div>{{ counter }}</div>
+    <button @click="inc()">+</button>
+    <button @click="dec()">-</button>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Counter extends Vue {
+    counter = 0;
+
+    inc(): void {
+      this.counter += 1;
+    }
+
+    dec(): void {
+      this.counter -= 1;
+    }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,2 @@
-import HelloWorld from './components/HelloWorld.vue';
-
-export default HelloWorld;
+export * from './components/HelloWorld.vue';
+export * from './components/Counter.vue';
